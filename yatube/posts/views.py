b727463518804e981ab592f4ac0b26ca598c335d
Никита Сухарев <sukharev1998@yandex.ru>
2023-03-28 14:49:53 +0300

    исправлены ошибки

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -18,8 +18,7 @@ def page_look(post_list, page_number):
 
 def index(request):
     post_list = Post.objects.select_related('author', 'group')
-    page_number = request.GET.get('page')
-    page_obj = page_look(post_list, page_number)
+    page_obj = page_look(post_list, request.GET.get('page'))
     context = {
         'page_obj': page_obj,
     }
@@ -29,11 +28,9 @@ def index(request):
 def profile(request, username):
     author = get_object_or_404(User, username=username)
     author_posts = author.posts.select_related('group')
-    page_number = request.GET.get('page')
-    page_obj = page_look(author_posts, page_number)
+    page_obj = page_look(author_posts, request.GET.get('page'))
     context = {
         'author': author,
-        'author_posts': author_posts,
         'page_obj': page_obj,
     }
     return render(request, 'posts/profile.html', context)
@@ -41,7 +38,7 @@ def profile(request, username):
 
 def post_detail(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
-    posts_count = Post.objects.filter(author__exact=post.author).count
+    posts_count = post.author.posts.count()
     context = {
         "post": post,
         "posts_count": posts_count,
@@ -51,9 +48,8 @@ def post_detail(request, post_id):
 
 def group_list(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    post_list = group.posts.all()
-    page_number = request.GET.get('page')
-    page_obj = page_look(post_list, page_number)
+    post_list = group.posts.select_related("author")
+    page_obj = page_look(post_list, request.GET.get('page'))
     context = {
         'group': group,
         'page_obj': page_obj,
@@ -78,8 +74,6 @@ def post_create(request):
 def post_edit(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
     form = PostForm(request.POST or None, instance=post)
-    if post.author != request.user:
-        return redirect('posts:post_detail', post.id)
     if form.is_valid():
         post = form.save()
         form.instance.author = request.user
@@ -88,6 +82,5 @@ def post_edit(request, post_id):
     context = {
         'form': form,
         'is_edit': True,
-        'post': post,
     }
     return render(request, "posts/post_create.html", context)
